95042dae6e7e2c68def905f68884b0a98eff6ba2
OngJunKai <ongjunkai07@gmail.com>
2025-07-22 02:33:07 +0000

    message

---
+++ Part1/ex05.py
@@ -0,0 +1,13 @@
+# Word variables
+word1 = 'car '
+word2 = 'exceeded '
+text1 = 'speed limit '
+word3 = 'the '
+
+# : Combine the words into one sentence and display on screen
+# Make sure your sentence reads: "the car exceeded the speed limit"
+
+# Insert your code here
+apple = word1 + word2 + word3 + text1
+
+print(apple)
